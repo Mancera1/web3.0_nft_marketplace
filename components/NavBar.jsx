432e074505e5d940cbee8a7ebc38cb2c8bc7127d
Jose Mancera <mancerajose11@gmail.com>
2022-10-28 00:00:30 -0500

    Add assets folder with intended images

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import { useEffect, useState, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes'; /* To identify light or dark mode */
+import Link from 'next/link';
+import Image from 'next/image';
+import images from '../assets';
 
 const NavBar = () => (
   <div>NavBar</div>
